491980ff8d9a9d629318e70035f0067da6b5164b
Denis Stepulenok <super.denis@gmail.com>
2015-09-30 01:50:40 +0300

    Задания уровня 4 до 13 урока

---
+++ src/com/javarush/test/level04/lesson13/task05/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.test.level04.lesson13.task05;
+
+import java.util.Scanner;
+
+/* Все любят Мамбу
+Ввести с клавиатуры имя и используя цикл for 10 раз вывести: [*имя* любит меня.]
+Пример текста:
+Света любит меня.
+Света любит меня.
+…
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        for (int i = 0; i < 10; i++) {
+            System.out.println(name + " любит меня.");
+        }
+    }
+}
